b2dba084f59b972c602399c0c58040a03a3d06d4
JeMin <madunyao01@163.com>
2021-06-07 00:42:17 +0800

    模态框modal

--- src/components/base-ui/modal/modal.vue
+++ src/components/base-ui/modal/modal.vue
@@ -1,15 +1,28 @@
 <template>
+<div class="modal-wrap" v-show="showFlag">
   <div class="modal">
-    modal
+      <div class="modal-top">
+        <a class="fa fa-times" href="" @click.prevent="closeModal"></a>
+      </div>
+      <div class="modal-content">
+        <slot name="content"></slot>
+      </div>
+      <div class="buts-wrap" v-if="option && !option.hideButton">
+        <button class="j-yellow-h" @click="submitModal">确定</button>
+        <button class="j-gray-h" @click="closeModal">关闭</button>
+      </div>
   </div>
+</div>
 </template>
 
 <script type="text/javascript">
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
+import { ref } from 'vue';
 
 export default {
   name: 'modal',
+  props: ['option'],
   data() {
     return {
 
@@ -17,8 +30,67 @@ export default {
   },
   components: {
   },
+  setup(props) {
+    const showFlag = ref(false);
+    function closeModal() {
+      showFlag.value = false;
+    }
+    function openModal() {
+      showFlag.value = true;
+    }
+    function submitModal() {
+      if (props.option.submit) {
+        props.option.submit();
+      }
+      showFlag.value = false;
+    }
+    return {
+      closeModal,
+      submitModal,
+      openModal,
+      showFlag,
+    };
+  },
 };
 </script>
 
-<style  scoped>
+<style lang="scss" scoped>
+.modal-wrap{
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 10000;
+    width: 100vw;
+    height: 100vh;
+    left: 0;
+    top: 0;
+    background-color: rgba(0,0,0,0.6);
+    .modal{
+      background: #fff;
+      min-width: 30%;
+      max-width: 50%;
+      padding: 20px 15px;
+      padding-top: 30px;
+      margin-bottom: 10%;
+      position: relative;
+    }
+}
+.modal-wrap .modal .modal-content{
+  padding: 5px;
+  min-height: 80px;
+}
+.modal-wrap .modal .modal-top{
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: flex-end;
+    height: 25px;
+    width: 100%;
+    a{
+      font-size: 15px;
+      margin: 5px 5px 0 0;
+    }
+}
 </style>
--- src/views/modalView.vue
+++ src/views/modalView.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="modalView">
-     modal view
-     <modal></modal>
-     <input type="text">
+     <div class="buts-wrap">
+       <span >基础模态框：</span>
+        <button class="j-blue-h" @click="showBasicModal()">显示Modal</button>
+     </div>
+     <!-- 基础模态框 -->
+     <modal ref="modalRef" :option="modalOption">
+        <template v-slot:content >
+          请确认信息是否正确？
+          <p>
+            用户名：张三
+          </p>
+      </template>
+     </modal>
   </div>
 </template>
 
 <script type="text/javascript">
 // @ is an alias to /src
 import modal from '@/components/base-ui/modal/modal.vue';
+import { ref, reactive } from 'vue';
 
 export default {
   name: 'modalView',
@@ -20,8 +31,33 @@ export default {
   components: {
     modal,
   },
+  setup() {
+    const modalRef = ref(null);
+    const modalOption = reactive({
+      hideButton: false,
+      submit() {
+        console.log('提交模态框成功！');
+      },
+    });
+    function showBasicModal() {
+      modalRef.value.openModal();
+    }
+    return {
+      showBasicModal,
+      modalRef,
+      modalOption,
+    };
+  },
 };
 </script>
 
-<style  scoped>
+<style lang="scss" scoped>
+  .buts-wrap{
+    justify-content:
+    flex-start;margin:20px;
+    span{
+      font-size: 1rem;
+      line-height: 2.5rem;
+    }
+  }
 </style>
